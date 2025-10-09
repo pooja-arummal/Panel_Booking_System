7894af3d900e7bc0b50f574cd04cb8c0c9e81624
pooja-arummal <poojaarummal08@gmail.com>
2025-10-09 17:48:17 +0530

    dashboard v1

--- src/Components/dashboard.jsx
+++ src/Components/dashboard.jsx
@@ -1,4 +1,7 @@
-import "./dashboard.css"
+
+import "./dash.css";
+import cutepresenter from "./cute-presenter.jpg"
+
 const DashBoard = () =>{
     return(
     <div className="dashboard">
@@ -7,12 +10,24 @@ const DashBoard = () =>{
         </div>
       
         <div className="dash">DashBoard</div>
-        <div className="block">
+        <div className="content">
+          <div className="block">
         <div className="options">Available slot</div>
         <div className="options">Pending Presentation</div>
-        </div>
-        <div className="options">Project Panel Group</div>
+         <div className="options">Project Panel Group</div>
         <div className="options">Guide Student list</div>
+        <div className="options">Available Venues</div>
+        </div>
+
+        <div className="Image">
+          {/* <img src = ""> */}
+          <img src = {cutepresenter}/>
+        </div>
+       
+        </div>
+
+
+        
       {/* <Button/> */}
             
     </div>
@@ -20,3 +35,5 @@ const DashBoard = () =>{
 }
 
 export default DashBoard
+
+
